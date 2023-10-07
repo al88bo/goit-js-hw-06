daedb37ce06d4db0eef25af96ebac4e21374d55d
Oleksii Bohoslavskyi <al88bo@gmail.com>
2023-10-07 09:39:03 +0300

    All 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,30 @@
+
+// А МОЖНА ТАК?
+
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+createBtn.addEventListener('click', actionsWithButtons);
+destroyBtn.addEventListener('click', actionsWithButtons);
+
+function actionsWithButtons(e) {
+  if (e.target.hasAttribute('data-destroy'))
+    return e.target.parentNode.nextElementSibling.innerHTML = '';
+  const markup = Array.from({ length: +e.target.previousElementSibling.value }, (_, i) =>
+    `<div style='width: ${30 + i * 10}px; height: ${30 + i * 10}px; background-color: ${getRandomHexColor()}'></div>`)
+    .join('');
+  return e.target.parentNode.nextElementSibling.innerHTML = markup;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+  }
+
+// function createBoxes(amount) {
+  
+// }
+  
+// function destroyBoxes() {
+  
+// }
